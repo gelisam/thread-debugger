2e0e89f225e05be2652e3543df21c2a5bbc2d720
Samuel Gélineau <gelisam+github@gmail.com>
2022-11-03 17:06:28 -0400

    hello haskell-gi

--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,18 @@
+{-# LANGUAGE OverloadedStrings, OverloadedLabels #-}
 module Main where
 
+import Data.GI.Base
+import qualified GI.Gtk as Gtk
+
+
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = do
+  Gtk.init Nothing
+  win <- new Gtk.Window [ #title := "Hi there" ]
+  on win #destroy Gtk.mainQuit
+  button <- new Gtk.Button [ #label := "Click me" ]
+  on button #clicked (set button [ #sensitive := False,
+                                   #label := "Thanks for clicking me" ])
+  #add win button
+  #showAll win
+  Gtk.main
