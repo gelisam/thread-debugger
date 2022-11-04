1029828c07f05b0dda6b4a6e39a75fad6409eaf1
Samuel Gélineau <gelisam+github@gmail.com>
2022-11-03 22:44:49 -0400

    GlobalState

--- app/Main.hs
+++ app/Main.hs
@@ -1,11 +1,16 @@
-{-# LANGUAGE LambdaCase, OverloadedLabels, OverloadedStrings, RecordWildCards #-}
+{-# LANGUAGE DeriveGeneric, LambdaCase, OverloadedLabels, OverloadedStrings, RecordWildCards #-}
 module Main where
 
+import Control.Concurrent.MVar
 import Data.Foldable (for_)
 import Data.GI.Base
 import Data.Map (Map)
 import Data.String (IsString(fromString))
+import Data.Traversable (for)
+import GHC.Generics (Generic)
+import Optics
 import qualified Control.Monad.Trans.State as State
+import qualified GI.Gdk.Constants as Gdk
 import qualified GI.Gtk as Gtk
 import qualified Data.Map as Map
 
@@ -20,8 +25,11 @@ exampleInput
     , "B baz"
     ]
 
+type Key
+  = Maybe String
+
 type Entry
-  = (Maybe String, String)
+  = (Key, String)
 
 type History
   = [Entry]
@@ -45,22 +53,72 @@ parseHistory
 data Widgets = Widgets
   { widgetsWindow
       :: Gtk.Window
-  , widgetsButton
-      :: Gtk.Button
+  , widgetsListBox
+      :: Gtk.ListBox
   }
+  deriving Generic
 
-type Windows
-  = Map (Maybe String) Widgets
+data RenderedEntry = RenderedEntry
+  { renderedEntryItself
+      :: Entry
+  , renderedEntryLabel
+      :: Gtk.Label
+  }
+  deriving Generic
+
+data Zipper a = Zipper [a] a [a]
+
+appendZipper
+  :: a
+  -> Zipper a
+  -> Zipper a
+appendZipper new (Zipper xs y zs)
+  = Zipper (y:xs) new zs
+
+data GlobalState = GlobalState
+  { globalStateMainWindow
+      :: Widgets
+  , globalStateThreadWindows
+      :: Map String Widgets
+  , globalStateRenderedEntries
+      :: Zipper RenderedEntry
+  }
+  deriving Generic
+
+getListBox
+  :: MVar GlobalState
+  -> Key
+  -> IO Gtk.ListBox
+getListBox mvar k = do
+  withMVar mvar $ \(GlobalState {..}) -> do
+    pure $ widgetsListBox $ globalStateMainWindow
 
 mainWindowKeyPressCallback :: Gtk.WidgetKeyPressEventCallback
 mainWindowKeyPressCallback eventKey = do
-  eventString <- get eventKey #string
-  case eventString of
-    Just "\ESC" -> do
+  eventKeyval <- get eventKey #keyval
+  case eventKeyval of
+    Gdk.KEY_Escape -> do
       Gtk.mainQuit
       pure True
+    Gdk.KEY_Left -> do
+      putStrLn "left"
+      pure True
     _ -> do
+      --print eventKeyval
       pure False
+
+newGlobalState :: Widgets -> [RenderedEntry] -> IO (MVar GlobalState)
+newGlobalState mainWindowWidgets renderedEntries = do
+  let lastEntry : reversedEntries = reverse renderedEntries
+
+  newMVar $ GlobalState
+    { globalStateMainWindow
+        = mainWindowWidgets
+    , globalStateThreadWindows
+        = Map.empty
+    , globalStateRenderedEntries
+        = Zipper reversedEntries lastEntry []
+    }
 
 main :: IO ()
 main = do
@@ -76,7 +134,24 @@ main = do
   listBox <- new Gtk.ListBox []
   #add scrolledWindow listBox
 
-  for_ exampleInput $ \s -> do
+  renderedEntries <- for exampleInput $ \s -> do
     label <- new Gtk.Label [ #label := fromString s ]
     Gtk.listBoxInsert listBox label (-1)
+    pure $ RenderedEntry
+      { renderedEntryItself
+          = (Nothing, s)
+      , renderedEntryLabel
+          = label
+      }
+
+  let mainWindowWidgets :: Widgets
+      mainWindowWidgets = Widgets
+        { widgetsWindow
+            = mainWindow
+        , widgetsListBox
+            = listBox
+        }
+  mvar <- newGlobalState mainWindowWidgets renderedEntries
   #showAll mainWindow
+
+  Gtk.main
